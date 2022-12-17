4f16a5e6be445408ecfd9b5f36e9104b61b9e5ca
REOfazo <94753088+REOfazo@users.noreply.github.com>
2022-12-17 10:30:48 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function showMaruza() {
+    document.getElementById("id1").style.display = 'block';
+}
+function showVideo1() {
+    document.getElementById("video1").style.display = 'block';
+    document.getElementById("video2").style.display = 'none';
+    document.getElementById("video3").style.display = 'none';
+    document.getElementById("video4").style.display = 'none';
+    document.getElementById("video5").style.display = 'none';
+    document.getElementById("id1").style.display = 'none';
+}
+function showVideo2() {
+    document.getElementById("video1").style.display = 'none';
+    document.getElementById("video2").style.display = 'block';
+    document.getElementById("video3").style.display = 'none';
+    document.getElementById("video4").style.display = 'none';
+    document.getElementById("video5").style.display = 'none';
+    document.getElementById("id1").style.display = 'none';
+}
+function showVideo3() {
+    document.getElementById("video1").style.display = 'none';
+    document.getElementById("video2").style.display = 'none';
+    document.getElementById("video3").style.display = 'block';
+    document.getElementById("video4").style.display = 'none';
+    document.getElementById("video5").style.display = 'none';
+    document.getElementById("id1").style.display = 'none';
+}
+function showVideo4() {
+    document.getElementById("video1").style.display = 'none';
+    document.getElementById("video2").style.display = 'none';
+    document.getElementById("video3").style.display = 'none';
+    document.getElementById("video4").style.display = 'block';
+    document.getElementById("video5").style.display = 'none';
+    document.getElementById("id1").style.display = 'none';
+}
+function showVideo5() {
+    document.getElementById("video1").style.display = 'none';
+    document.getElementById("video2").style.display = 'none';
+    document.getElementById("video3").style.display = 'none';
+    document.getElementById("video4").style.display = 'none';
+    document.getElementById("video5").style.display = 'block';
+    document.getElementById("id1").style.display = 'none';
+}
